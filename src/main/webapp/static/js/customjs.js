15f337a93cacc8031ebaffbbf076809435ebfe8e
Sushil <p12sushilkm@iimahd.ernet.in>
2014-11-29 14:52:54 +0530

    resolving loader issue in android

--- src/main/webapp/static/js/customjs.js
+++ src/main/webapp/static/js/customjs.js
@@ -17,18 +17,22 @@ $(window).load(function(){
 
 $(document).ready(function (){
 	$(".loader").remove();
-	$("a").click(function(){
+	$("a").click(function(e){
+		e.preventDefault();
 		var href = $(this).attr("href");
 		if(href=="" || href=="#"){
 			return;
 		}
 		loaderAnimation();
+		setTimeout(function(){
+	         window.location = href;
+	    },30);  
 	});
-	
+
 	$("form").submit(function(){
 		loaderAnimation();
 	});
-	
+
 	/*$(".header-nav-item").click(function(e){
 		$(".header-nav-item").removeClass("active");
 		$(this).addClass("active");
@@ -37,26 +41,26 @@ $(document).ready(function (){
 
 function getTenDigitPhoneNumber(phonenumber){
 	var tenDigit = /^\d{10}$/;  
-	  if(tenDigit.test(phonenumber))  
-	  {  
-	      return phonenumber;  
-	  }  
-	  else
-	  {   
-		  // Return only digits without any special character
-		  var phoneno = phonenumber.replace(/[^0-9\s]/gi, '').replace(/\s+/g, '');       
-		  var lengthOfPhoneno = phoneno.length;
-		  if(lengthOfPhoneno==10){
-			  return phoneno;
-		  }
-		  else if(lengthOfPhoneno>10){
-			  return phoneno.substring(lengthOfPhoneno-10);
-		  }
-		  else{
-			  return false;
-		  }
-		  
-	  }  
+	if(tenDigit.test(phonenumber))  
+	{  
+		return phonenumber;  
+	}  
+	else
+	{   
+		// Return only digits without any special character
+		var phoneno = phonenumber.replace(/[^0-9\s]/gi, '').replace(/\s+/g, '');       
+		var lengthOfPhoneno = phoneno.length;
+		if(lengthOfPhoneno==10){
+			return phoneno;
+		}
+		else if(lengthOfPhoneno>10){
+			return phoneno.substring(lengthOfPhoneno-10);
+		}
+		else{
+			return false;
+		}
+
+	}  
 }	
 
 function displayQuickNotification(message ,timeout){
@@ -65,24 +69,24 @@ function displayQuickNotification(message ,timeout){
 	$(divElem).addClass("quicknotification");
 	$(divElem).text(message).fadeIn("fast");
 	setTimeout( function(){
-        $(divElem).fadeOut("fast");
-        $(divElem).remove();
-    }, timeout);
+		$(divElem).fadeOut("fast");
+		$(divElem).remove();
+	}, timeout);
 }
-	 
+
 /*
 function validatePhonenumber(phonenumber){
 	var number = phonenumber.replace(/\s+/g, '');
 	var codeFromDropDown = $("li.active").find(".dial-code").text();
 	var lengthOfCode = codeFromDropDown.length;
 	var codeFromUserInput = number.substring(0,lengthOfCode);
-	
+
 	if(codeFromDropDown!=codeFromUserInput){
 		return false;
 	}
-	
+
 	var tenDigitNumber = number.substring(lengthOfCode);
-	
+
 	var phoneno = /^\d{10}$/;  
 	  if(phoneno.test(tenDigitNumber))  
 	  {  
@@ -92,7 +96,7 @@ function validatePhonenumber(phonenumber){
 	  {   
 	     return false;  
 	  }  
-	
+
 //	return codeFromUserInput +"<>" +number+"<>"+codeFromDropDown+"<>"+lengthOfCode;
 }
-*/
+ */
